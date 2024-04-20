f527b22a936b6693eba1932ed3a62805700b35f8
Banula Perera <93034784+banulaperera@users.noreply.github.com>
2024-04-20 11:36:07 +0530

    Complete implementation of the parser and the data structure.

--- src/Main.java
+++ src/Main.java
@@ -10,45 +10,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the path of the file: ");
+        System.out.println("\nWelcome to the Sliding Puzzle Game!");
+        System.out.println("\nPlease share the file path with me:");
         String path = scanner.nextLine();
 
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
         String line;
-        int heightOfTheMaze = 0;
-        int widthOfTheMaze = 0;
-        int startY = 0, startX = 0, finishY = 0, finishX = 0;
-        boolean valid = true;
+        int heightOfTheMaze = 0, widthOfTheMaze = 0;
+        int startPointY = 0, startPointX = 0, finishPointY = 0, finishPointX = 0;
+        boolean isValid = true, startPointFound = false, finishPointFound = false;
 
-        while ((line = bufferedReader.readLine()) != null) {
-            if (heightOfTheMaze == 0) {
-                widthOfTheMaze = line.length();
-            }
+        while ((line = bufferedReader.readLine()) != null && isValid) {
+            widthOfTheMaze = heightOfTheMaze == 0 ? line.length() : widthOfTheMaze;
             heightOfTheMaze++;
 
             for (int colX = 0; colX < widthOfTheMaze; colX++) {
                 char cell = line.charAt(colX);
                 if (cell == 'S') {
-                    startY = heightOfTheMaze - 1;
-                    startX = colX;
+                    startPointY = heightOfTheMaze - 1;
+                    startPointX = colX;
+                    startPointFound = true;
                 } else if (cell == 'F') {
-                    finishY = heightOfTheMaze - 1;
-                    finishX = colX;
+                    finishPointY = heightOfTheMaze - 1;
+                    finishPointX = colX;
+                    finishPointFound = true;
                 } else if (cell != '0' && cell != '.') {
-                    System.out.println("Invalid characters found in the maze format.");
-                    System.out.println("Starting position must have ’S’, ending position must have ‘F’, rocks are ‘0’ and ice is ‘.’.");
-                    valid = false;
+                    System.out.println("\nInvalid characters found in the maze format.");
+                    isValid = false;
                     break;
                 }
             }
-
-            if (!valid) {
-                break;
-            }
         }
 
-        if (valid) {
+        if (isValid && (!startPointFound || !finishPointFound)) {
+            System.out.println((!startPointFound ? "Starting" : "Ending") + " point is missing in the maze.");
+            isValid = false;
+        }
+
+        if (isValid) {
             String[][] maze = new String[heightOfTheMaze][widthOfTheMaze];
+
+            bufferedReader.close();
+            bufferedReader = new BufferedReader(new FileReader(path));
+
+            int row = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                maze[row++] = line.split("");
+            }
         }
     }
 }
