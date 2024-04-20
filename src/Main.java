bcf35ac0be573fce90736fc17f001ae99f1c3e23
Banula Perera <93034784+banulaperera@users.noreply.github.com>
2024-04-20 10:14:59 +0530

    implementation of the parser

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/*
+ * Student name - Banula Perera
+ * IIT Number - 20212085
+ * UOW Number - W1871527
+ */
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the path of the file: ");
+        String path = scanner.nextLine();
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        String line;
+        int heightOfTheMaze = 0;
+        int widthOfTheMaze = 0;
+        int startY = 0, startX = 0, finishY = 0, finishX = 0;
+        boolean valid = true;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            if (heightOfTheMaze == 0) {
+                widthOfTheMaze = line.length();
+            }
+            heightOfTheMaze++;
+
+            for (int colX = 0; colX < widthOfTheMaze; colX++) {
+                char cell = line.charAt(colX);
+                if (cell == 'S') {
+                    startY = heightOfTheMaze - 1;
+                    startX = colX;
+                } else if (cell == 'F') {
+                    finishY = heightOfTheMaze - 1;
+                    finishX = colX;
+                } else if (cell != '0' && cell != '.') {
+                    System.out.println("Invalid characters found in the maze format.");
+                    System.out.println("Starting position must have ’S’, ending position must have ‘F’, rocks are ‘0’ and ice is ‘.’.");
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (!valid) {
+                break;
+            }
+        }
+
+        if (valid) {
+            String[][] maze = new String[heightOfTheMaze][widthOfTheMaze];
         }
     }
 }
